ed54554dc69498b0dbcb6015931f59c62c67d933
Samyak Shreyash <sam.shreyash@gmail.com>
2021-09-16 13:59:13 +0530

    Fixed Date to subtract 1 month to show exact date duer to 0 indexing in new Date()

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,11 @@
 import "./ExpenseDate.css"
 function ExpenseDate(props)
 {
-    const day = props.date.toLocaleString('en-US',  { day: 'numeric'}); 
-    const month = props.date.toLocaleString('en-US',  { month: 'long'});
-    const year = props.date.toLocaleString('en-US',  { year: 'numeric'});
+    const date = props.date;
+    date.setMonth(date.getMonth()-1);
+    const day = date.toLocaleString('en-US',  { day: 'numeric'}); 
+    const month = date.toLocaleString('en-US',  { month: 'long'});
+    const year = date.toLocaleString('en-US',  { year: 'numeric'});
 
     return (
         <div className="expense-date">
