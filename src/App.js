658395d661a4260f21d3f1d5df6e54b40847f9b2
Samyak Shreyash <sam.shreyash@gmail.com>
2021-09-16 17:54:03 +0530

    Added Temporal lib to fix 1-indexing of Date library

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
+import { Temporal } from 'proposal-temporal';
+
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+
 function App() {
 	const expenses = [
 		{
 			id: 'i1',
 			title: 'Petrol',
 			amount: '3210',
-			date: new Date(2021, 9, 6)
+			date: new Temporal.PlainDate(2021, 9, 6)
 		},
 		{
 			id: 'i2',
 			title: 'Zomato',
 			amount: '310',
-			date: new Date(2021, 8, 26)
+			date: new Temporal.PlainDate(2021, 8, 26)
 		},
 		{
 			id: 'i3',
 			title: 'Wifi',
 			amount: '800',
-			date: new Date(2021, 9, 1)
+			date: new Temporal.PlainDate(2021, 9, 1)
 		},
 		{
 			id: 'i4',
 			title: 'Flight',
 			amount: '9820',
-			date: new Date(2021, 8, 16)
+			date: new Temporal.PlainDate(2021, 8, 16)
 		},
 	];
 
