845e95800d186af836b2cc12da2b9097c084f705
Samyak Shreyash <sam.shreyash@gmail.com>
2021-09-17 07:34:29 +0530

    Added filter to see Expense by Year'

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
-const expenses = (props) => {
+const Expenses = props => {
+	const currYear = new Date().getYear()+1900;
+	const [filteredYear, setFilteredYear] = useState(currYear.toString());
+
+	const filterChangeHandler = selectedYear => {
+		setFilteredYear(selectedYear);
+	};
+
 	return (
 		<Card>
+			<ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler} />
 			<ExpenseItem title={props.items[0].title} date={props.items[0].date} price={props.items[0].amount} />
 			<ExpenseItem title={props.items[1].title} date={props.items[1].date} price={props.items[1].amount} />
 			<ExpenseItem title={props.items[2].title} date={props.items[2].date} price={props.items[2].amount} />
@@ -13,4 +24,4 @@ const expenses = (props) => {
 	);
 };
 
-export default expenses;
+export default Expenses;
